a4d7bb9813c6c9d126382653c7e3be9180352a13
Pranav2335 <106325687+Pranav2335@users.noreply.github.com>
2022-12-22 07:29:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class Config(object):
      APP_ID = int(os.environ.get("APP_ID", ""))
      API_HASH = os.environ.get("API_HASH", "")
      #Array to store users who are authorized to use the bot
-     ADMIN = os.environ.get("ADMIN", "754495556")
+     ADMIN = os.environ.get("ADMIN", "1484847208")
      #Your Mongo DB Database Name
-     DB_NAME = os.environ.get("DB_NAME", "my")
+     DB_NAME = os.environ.get("DB_NAME", "Prv")
      #Your Mongo DB URL Obtained From mongodb.com
      DB_URL = os.environ.get("DB_URL", "")
